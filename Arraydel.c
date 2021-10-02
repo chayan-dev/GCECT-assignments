2a75d6a4f3b53b72f69949ea69094bd188e8802e
Priyanshu Naskar <71266237+pnaskardev@users.noreply.github.com>
2021-10-02 23:48:58 +0530

    main

--- Arraydel.c
+++ Arraydel.c
@@ -1,40 +1,149 @@
-#include<stdio.h>
-#include<stdlib.h>
-void delete(int arr[],int idx,int *leng)
+#include <stdio.h>
+#include <stdlib.h>
+#include<strings.h>
+#include<string.h>
+struct Node
 {
-    int x=0;
-    if(idx>=0 && idx<*leng-1)
-    {
-        x=arr->a[idx];
-        for(int i=idx;i<arr->leng-1;i++)
-        {
-            arr->a[i]=arr->a[i+1];
-            arr->leng--;
-        }
-    }
-    else if(idx==arr->leng-1)
-    {
-        arr->leng--;
-    }
+ int data;
+ struct Node *next;
+}*top=NULL;
+void push(int x)
+{
+ struct Node *t;
+ t=(struct Node*)malloc(sizeof(struct Node));
+
+ if(t==NULL)
+ printf("stack is full\n");
+ else
+ {
+ t->data=x;
+ t->next=top;
+ top=t;
+ }
+
 }
-int main(void)
+int pop()
 {
-    int n;
-    printf("Enter the size of the array:");
-    scanf("%d",&n);
-    int arr[n];
-    printf("Enter elements of the array:\n");
-    for(int i=0;i<n;i++)
-    {
-        scanf("%d",&arr[i]);
-    }
-    int leng=sizeof(n)/sizeof(int);
-    int idx;
-    printf("Enter the index where the element shall be deleted:=");
-    scanf("%d",&idx);
-    delete(&arr,idx);
-    for(int i=0;i<=leng-1;i++)
-    {
-        printf("%d ",arr[i]);
-    }
+ struct Node *t;
+ int x=-1;
+
+ if(top==NULL)
+ printf("Stack is Empty\n");
+ else
+ {
+ t=top;
+ top=top->next;
+ x=t->data;
+ free(t);
+ }
+ return x;
+}
+void Display()
+{
+ struct Node *p;
+ p=top;
+ while(p!=NULL)
+ {
+ printf("%d ",p->data);
+ p=p->next;
+ }
+ printf("\n");
+}
+int isBalanced(char *exp)
+{
+ int i;
+
+ for(i=0;exp[i]!='\0';i++)
+ {
+ if(exp[i]=='(')
+ push(exp[i]);
+ else if(exp[i]==')')
+ {
+ if(top==NULL)
+ return 0;
+ pop();
+ }
+ }
+ if(top==NULL)
+ return 1;
+ else
+ return 0;
+}
+int pre(char x)
+{
+ if(x=='+' || x=='-')
+ return 1;
+ else if(x=='*' || x=='/')
+ return 2;
+ return 0;
+}
+
+int isOperand(char x)
+{
+ if(x=='+' || x=='-' || x=='*' || x=='/')
+ return 0;
+ else
+ return 1;
+
+}
+
+char * InToPost(char *infix)
+{
+ int i=0,j=0;
+ char *postfix;
+ long len=strlen(infix);
+ postfix=(char *)malloc((len+2)*sizeof(char));
+
+ while(infix[i]!='\0')
+ {
+ if(isOperand(infix[i]))
+ postfix[j++]=infix[i++];
+ else
+ {
+ if(pre(infix[i])>pre(top->data))
+ push(infix[i++]);
+ else
+ {
+ postfix[j++]=pop();
+ }
+ }
+ }
+ while(top!=NULL)
+ postfix[j++]=pop();
+ postfix[j]='\0';
+ return postfix;
+}
+int Eval(char *postfix)
+{
+ int i=0;
+ int x1,x2,r=0 ;
+
+ for(i=0;postfix[i]!='\0';i++)
+ {
+ if(isOperand(postfix[i]))
+ {
+ push(postfix[i]-'0');
+ }
+ else
+ {
+ x2=pop();x1=pop();
+ switch(postfix[i])
+ {
+ case '+':r=x1+x2; break;
+ case '-':r=x1-x2; break;
+ case '*':r=x1*x2; break;
+ case '/':r=x1/x2; break;
+ }
+ push(r);
+ }
+ }
+ return top->data;
+}
+int main()
+{
+ char *postfix="6924+-*";
+
+ printf("Result is %d\n",Eval(postfix));
+
+ return 0;
 }
